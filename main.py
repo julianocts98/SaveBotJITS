29c467b4098341932c0499f8cbc74866107accee
Juliano <juliano.cavalcante1998@hotmail.com>
2020-06-13 15:11:14 -0300

    Enfatiza a heresia no ato do ser que deletou o chat

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ discord.opus.load_opus("libopus.so.0")
 @client.event
 async def on_ready():
     print('Logado como {0.user}'.format(client))
+    #joao = discord.utils.find(lambda m: m.name == 'João Vittor', client.get_guild(98846974278664192))
+    #print(joao)
 
 @client.event
 async def on_message(message):
@@ -24,9 +26,13 @@ async def on_message(message):
     # print(message.content)
     # print(message.created_at)
 
+    # if message.content.startswith("!info"):
+    #     print(message.author.guild.members)
+    #     with open("membros", "w") as file:
+    #          file.writelines(str(message.author.guild.members))
+
     if message.content.startswith("!!!tururu"):
         voice_client = await message.author.voice.channel.connect()
-
         def my_after(error):
             coro = voice_client.disconnect()
             fut = asyncio.run_coroutine_threadsafe(coro, client.loop)
@@ -38,4 +44,21 @@ async def on_message(message):
         source = discord.FFmpegOpusAudio("audios/tururu.mp3")
         voice_client.play(source, after=my_after)
 
+@client.event
+async def on_voice_state_update(member, before, after):
+    if member.id == 110036152009912320 and before.channel==None: #id do usuario que deletou o chat
+        for channel in client.get_all_channels():
+            if type(channel) == discord.channel.TextChannel:
+                await channel.send(file=discord.File("imgs/shamegot.jpg"))
+        voice_client = await after.channel.connect()
+        def my_after(error):
+            coro = voice_client.disconnect()
+            fut = asyncio.run_coroutine_threadsafe(coro, client.loop)
+            try:
+                fut.result()
+            except:
+                # an error happened sending the message
+                pass
+        source = discord.FFmpegOpusAudio("audios/shame.mp3")
+        voice_client.play(source, after=my_after)
 client.run(aux.get_token())
