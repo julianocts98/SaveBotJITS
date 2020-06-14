6be4384c407ea3eec7096ca51f63a498b621c8ad
Juliano <juliano.cavalcante1998@hotmail.com>
2020-06-14 16:40:52 -0300

    Adicionado play do Youtube

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+from __future__ import unicode_literals
 import discord
 import aux
 import asyncio
-import logger
+import youtube_dl
+
 
 client = discord.Client()
 
@@ -12,7 +14,7 @@ discord.opus.load_opus("libopus.so.0")
 @client.event
 async def on_ready():
     print('Logado como {0.user}'.format(client))
-    
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -23,6 +25,22 @@ async def on_message(message):
             for voclient in client.voice_clients:
                 if voclient.channel == message.author.voice.channel:
                     await voclient.disconnect()
+
+    if message.content.startswith("-shame"):
+        channel = message.channel
+        await channel.send(file=discord.File("imgs/shamegot.jpg"))
+        voice_client = await message.author.voice.channel.connect()
+        def my_after(error):
+            coro = voice_client.disconnect()
+            fut = asyncio.run_coroutine_threadsafe(coro, client.loop)
+            try:
+                fut.result()
+            except:
+                # an error happened sending the message
+                pass
+        source = discord.FFmpegOpusAudio("audios/shame.mp3")
+        voice_client.play(source, after=my_after)
+
 
     if any(palavra in message.content for palavra in ["-cheiro", "-pneu"]):
         channel = message.channel
@@ -38,7 +56,12 @@ async def on_message(message):
         except asyncio.TimeoutError:
             await channel.send("Achei que era comigo ;-;")
         else:
-            await channel.send("Para parar de tocar, digite \"-jstop\" :) ")
+            if ( msg.content.startswith("1") or msg.content.startswith("2") ):
+                await channel.send("Para parar de tocar, digite \"-jstop\" :) ")
+            else:
+                await channel.send("Infelizmente não tenho tantas opções de pneu queimado :(")
+                await channel.send("Use o comando -cheiro ou -pneu novamente caso queira ouvir alguma das disponíveis :) ")
+
 
         if msg.content.startswith("1"):
             voice_client = await message.author.voice.channel.connect()
@@ -80,14 +103,27 @@ async def on_message(message):
         source = discord.FFmpegOpusAudio("audios/tururu.mp3")
         voice_client.play(source, after=my_after)
 
-@client.event
-async def on_voice_state_update(member, before, after):
+    if message.content.startswith("-yt"):
+        conteudo = message.content[4:]
+        channel = message.channel
 
-    if member.id == 110036152009912320 and before.channel==None: #id do usuario que deletou o chat
-        for channel in client.get_all_channels():
-            if type(channel) == discord.channel.TextChannel:
-                await channel.send(file=discord.File("imgs/shamegot.jpg"))
-        voice_client = await after.channel.connect()
+        url = conteudo
+
+        ydl_opts = {
+            'default_search' : 'ytsearch',
+            'format': 'bestaudio/best',
+            'postprocessors':
+                [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                }],
+            'outtmpl' : 'audios/youtube-dl.%(ext)s'
+        }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+            
+        voice_client = await message.author.voice.channel.connect()
         def my_after(error):
             coro = voice_client.disconnect()
             fut = asyncio.run_coroutine_threadsafe(coro, client.loop)
@@ -96,7 +132,7 @@ async def on_voice_state_update(member, before, after):
             except:
                 # an error happened sending the message
                 pass
-        source = discord.FFmpegOpusAudio("audios/shame.mp3")
+        source = discord.FFmpegOpusAudio("audios/youtube-dl.mp3")
         voice_client.play(source, after=my_after)
 
 #executa o bot
